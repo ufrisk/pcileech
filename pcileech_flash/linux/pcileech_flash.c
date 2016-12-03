979e842f1d5a6cf8c4f5a88f81c8fb7d7992475a
ufrisk <github_ufrisk@frizk.net>
2016-12-03 14:42:26 +0100

    Firmware flash improvements.

--- pcileech_flash/linux/pcileech_flash.c
+++ pcileech_flash/linux/pcileech_flash.c
@@ -10,8 +10,9 @@
 // Usage:
 //  - Insert PCILeech card in computer via PCIe/mPCIe/ExpressCard/Thunderbolt (not USB)
 //  - run 'insmod pcileech_flash.ko'.   If the module is successfully inserted then the
-//    flash operation was successful. Run 'rmmod pcileech_flash' to clean up the module
-//    from the kernel. In order to enable the PCILeech functionality the device must be
+//    flash operation was successful.   If flashing the USB3380-EVB device the blue LED
+//    will be lit upon success.  Run 'rmmod pcileech_flash' to clean up the module from
+//    the kernel.      In order to enable the PCILeech functionality the device must be
 //    removed from (re-inserted into) the computer.
 //  - If flashing fails; please check 'dmesg' for logs.  If you are flashing the PP3380
 //    please ensure that the J3 jumper is bridged.  If it fails for unknown reasons try
@@ -31,52 +32,90 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ulf Frisk");
 MODULE_DESCRIPTION("PCILeech Firmware Automatic Flasher");
 
-#define OFFSET_PCIE_CONFIG	0x1000
-#define OFFSET_REG_EEPROM_CTL	0x260
-#define OFFSET_REG_EEPROM_DATA  0x264
-#define DEVICE_WAIT_TIME	10
+// -----------------------------------------------------------------------------
+// START SHARED CODE WITH WINDOWS/LINUX FLASH PCILEECH FIRMWARE
+// -----------------------------------------------------------------------------
 
-static int _action_flash_write(unsigned char *pbar0)
+#define OFFSET_USBREG_GPIO			0x50
+#define OFFSET_PCIREG_VEN_DEV		0x1000
+#define OFFSET_PCIREG_SUBSYS		0x102c
+#define OFFSET_PCIREG_EEPROM_CTL	0x1260
+#define OFFSET_PCIREG_EEPROM_DATA	0x1264
+#define DEVICE_WAIT_TIME			10
+#define SET_LED(v)					*(unsigned int*)(pbar0 + OFFSET_USBREG_GPIO) = (0x0000000f & v) | 0xf0
+
+static const unsigned char g_firmware_pcileech[] = {
+	0x5a, 0x00, 0x2a, 0x00, 0x23, 0x10, 0x49, 0x38, 0x00, 0x00, 0x00, 0x00, 0xe4, 0x14, 0xbc, 0x16,
+	0xc8, 0x10, 0x02, 0x06, 0x04, 0x00, 0xd0, 0x10, 0x84, 0x06, 0x04, 0x00, 0xd8, 0x10, 0x86, 0x06,
+	0x04, 0x00, 0xe0, 0x10, 0x88, 0x06, 0x04, 0x00, 0x21, 0x10, 0xd1, 0x18, 0x01, 0x90, 0x00, 0x00 };
+
+static int _action_flash_verify(unsigned char *pbar0)
 {
-	const unsigned char firmware_pcileech[] = {
-		0x5a, 0x00, 0x2a, 0x00, 0x23, 0x10, 0x49, 0x70, 0x00, 0x00, 0x00, 0x00, 0xe4, 0x14, 0xbc, 0x16,
-		0xc8, 0x10, 0x02, 0x06, 0x04, 0x00, 0xd0, 0x10, 0x84, 0x06, 0x04, 0x00, 0xd8, 0x10, 0x86, 0x06,
-		0x04, 0x00, 0xe0, 0x10, 0x88, 0x06, 0x04, 0x00, 0x21, 0x10, 0xd1, 0x18, 0x01, 0x90, 0x00, 0x00 };
-	unsigned int dwaddr, dwdata;
-	// write firmware loop
-	dwaddr = 0;
-	while(dwaddr < sizeof(firmware_pcileech)) {
-		// write enable latch (and wait for device)
-		*(unsigned char*)(pbar0 + OFFSET_PCIE_CONFIG + OFFSET_REG_EEPROM_CTL + 1) = 0xc0;
-		msleep(DEVICE_WAIT_TIME);
-		*(unsigned char*)(pbar0 + OFFSET_PCIE_CONFIG + OFFSET_REG_EEPROM_CTL + 1) = 0x00;
-		msleep(DEVICE_WAIT_TIME);
-		// write EEPROM data
-		dwdata = *(unsigned int*)(firmware_pcileech + dwaddr);
-		*(unsigned int*)(pbar0 + OFFSET_PCIE_CONFIG + OFFSET_REG_EEPROM_DATA) = dwdata;
-		// write to CTL register to start EEPROM write (and wait for device)
-		dwdata = *(unsigned int*)(pbar0 + OFFSET_PCIE_CONFIG + OFFSET_REG_EEPROM_CTL);
-		dwdata = (0x00ff0000 & dwdata) | 0x03004000 | (dwaddr >> 2);
-		*(unsigned int*)(pbar0 + OFFSET_PCIE_CONFIG + OFFSET_REG_EEPROM_CTL) = dwdata;
-		msleep(DEVICE_WAIT_TIME);
-		// next DWORD
-		dwaddr += 4;
-	}
-	// verify firmware loop
-	dwaddr = 0;
-	while(dwaddr < sizeof(firmware_pcileech)) {
+	unsigned int dwdata, dwaddr = 0;
+	while(dwaddr < sizeof(g_firmware_pcileech)) {
 		// write to CTL register to start EEPROM read (and wait for device)
-		dwdata = *(unsigned int*)(pbar0 + OFFSET_PCIE_CONFIG + OFFSET_REG_EEPROM_CTL);
+		dwdata = *(unsigned int*)(pbar0 + OFFSET_PCIREG_EEPROM_CTL);
 		dwdata = (0x00ff0000 & dwdata) | 0x00006000 | (dwaddr >> 2);
-		*(unsigned int*)(pbar0 + OFFSET_PCIE_CONFIG + OFFSET_REG_EEPROM_CTL) = dwdata;
+		*(unsigned int*)(pbar0 + OFFSET_PCIREG_EEPROM_CTL) = dwdata;
 		msleep(DEVICE_WAIT_TIME);
-		if( *(unsigned int*)(pbar0 + OFFSET_PCIE_CONFIG + OFFSET_REG_EEPROM_DATA) != *(unsigned int*)(firmware_pcileech + dwaddr)) {
-			return -EIO;
+		if(*(unsigned int*)(pbar0 + OFFSET_PCIREG_EEPROM_DATA) != *(unsigned int*)(g_firmware_pcileech + dwaddr)) {
+			return -1;
 		}
 		dwaddr += 4;
 	}
 	return 0;
 }
+
+static void _action_flash_write(unsigned char *pbar0)
+{
+	unsigned int dwdata, dwaddr = 0;
+	while(dwaddr < sizeof(g_firmware_pcileech)) {
+		// write enable latch (and wait for device)
+		*(unsigned char*)(pbar0 + OFFSET_PCIREG_EEPROM_CTL + 1) = 0xc0;
+		msleep(DEVICE_WAIT_TIME);
+		*(unsigned char*)(pbar0 + OFFSET_PCIREG_EEPROM_CTL + 1) = 0x00;
+		msleep(DEVICE_WAIT_TIME);
+		// write EEPROM data
+		dwdata = *(unsigned int*)(g_firmware_pcileech + dwaddr);
+		*(unsigned int*)(pbar0 + OFFSET_PCIREG_EEPROM_DATA) = dwdata;
+		// write to CTL register to start EEPROM write (and wait for device)
+		dwdata = *(unsigned int*)(pbar0 + OFFSET_PCIREG_EEPROM_CTL);
+		dwdata = (0x00ff0000 & dwdata) | 0x03004000 | (dwaddr >> 2);
+		*(unsigned int*)(pbar0 + OFFSET_PCIREG_EEPROM_CTL) = dwdata;
+		msleep(DEVICE_WAIT_TIME);
+		// next DWORD
+		dwaddr += 4;
+	}
+}
+
+static int _action_flash_writeverify(unsigned char *pbar0)
+{
+	// 1: check if this is a valid device / memory range.
+	if(*(unsigned int*)(pbar0 + OFFSET_PCIREG_SUBSYS) != 0x338010B5) {
+		return -1;
+	}
+	if(*(unsigned int*)(pbar0 + OFFSET_PCIREG_VEN_DEV) != 0x338010B5 && *(unsigned int*)(pbar0 + OFFSET_PCIREG_VEN_DEV) != 0x16BC14E4) {
+		return -1;
+	}
+	// 2: is firmware already flashed?
+	if(0 == _action_flash_verify(pbar0)) {
+		SET_LED(0xf); // success -> blue+red led
+		return 0;
+	}
+	// 3: flash firmware.
+	_action_flash_write(pbar0);
+	// 4: verify flashed firmware.
+	if(0 == _action_flash_verify(pbar0)) {
+		SET_LED(0x8); // success -> blue led
+		return 0;
+	}
+	SET_LED(0x7); // fail -> red led
+	return -1;
+}
+
+// -----------------------------------------------------------------------------
+// END SHARED CODE WITH WINDOWS/LINUX FLASH PCILEECH FIRMWARE
+// -----------------------------------------------------------------------------
 
 static int _action_flash_2(struct pci_dev *pdev)
 {
@@ -100,9 +139,9 @@ static int _action_flash_2(struct pci_dev *pdev)
 	}
 	// remap BAR0 avoiding the use of CPU cache
 	pbar0 = ioremap_nocache(pci_resource_start(pdev, 0), pci_resource_len(pdev, 0));
-	ret = _action_flash_write(pbar0);
+	ret = _action_flash_writeverify(pbar0);
 	if(ret) {
-		ret = _action_flash_write(pbar0); // make two write attempts
+		ret = _action_flash_writeverify(pbar0); // make two write attempts
 	}
 	iounmap(pbar0);
 	if(ret) {
--- readme.md
+++ readme.md
@@ -26,7 +26,7 @@ Please note that other adapters may also work.
 
 Flashing Hardware:
 ===============================
-In order to turn the USB3380 development board into a PCILeech device it needs to be flashed. Flashing must be done in Linux as root. Download the source code for the flash kernel module to build. The files are found in the pcileech_flash folder and are named: pcileech_flash.c and Makefile. The card must be connected to the Linux system doing the flashing via PCIe.
+In order to turn the USB3380 development board into a PCILeech device it needs to be flashed. Flashing must be done in Linux as root. Download the source code for the flash kernel module to build. The files are found in the pcileech_flash/linux folder and are named: pcileech_flash.c and Makefile. The card must be connected to the Linux system doing the flashing via PCIe.
 
 NB! If flashing the PP3380 PCIe card the J3 jumper must be bridged to connect the EEPROM. This is not necessary for the USB3380-EVB mini-PCIe card.
 
@@ -37,7 +37,7 @@ NB! If flashing the PP3380 PCIe card the J3 jumper must be bridged to connect th
 
 The insmod command must be run as root. If compilation fails you might have to install dependencies before you try again. On debian based systems - such as debian, ubuntu and kali, run ` apt-get update && apt-get install gcc make linux-headers-$(uname -r) ` and try again.
 
-If module insertion is successful flashing is also successful. In order to activate the flashed PCILeech device it must be power-cycled. Re-inserting it in the computer will achieve this. If one wish to flash more devices then unload the pcileech_flash kernel module by issuing the command: ` rmmod pcileech_flash `. If there is an error flashing is unsuccessful. Please try again and check any debug error messages by issing the command: ` dmsg `.
+If module insertion is successful flashing is also successful. If flashing the USB3380-EVB the blue LED will be lit upon success. In order to activate the flashed PCILeech device it must be power-cycled. Re-inserting it in the computer will achieve this. If one wish to flash more devices then unload the pcileech_flash kernel module by issuing the command: ` rmmod pcileech_flash `. If there is an error flashing is unsuccessful. Please try again and check any debug error messages by issing the command: ` dmsg `.
 
 Installing PCILeech:
 ====================
@@ -138,3 +138,4 @@ v1.2
 latest
 * new implant: spawn cmd in user context [wx64_pscmd_user]
 * implant: stability improvements for Win8+ [wx64_pscreate, wx64_pscmd, wx64_pscmd_user]
+* other: firmware flash improvements.
